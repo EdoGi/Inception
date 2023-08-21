8a798e4ceae81549d5dec4d4c6f0b4c3e9123567
Edoardo Giacomini <giacomini.ed@gmail.com>
2023-08-21 18:17:45 +0200

    add good version for MAC

--- MAC_VERSION/srcs/requirements/mariadb/tools/mariadb_setup.sh
+++ MAC_VERSION/srcs/requirements/mariadb/tools/mariadb_setup.sh
@@ -1,41 +1,39 @@
-#!/bin/sh
+#!/bin/bash
 
-mysql_install_db
-
-/etc/init.d/mysql start
-
-#Check if the database exists
+# This script sets up MySQL database and user privileges.
 
 if [ -d "/var/lib/mysql/$MYSQL_DATABASE" ]
 then 
 	echo "Database already exists"
 else
 
-# Set root option so that connexion without root password is not possible
+# Create an SQL script for initializing the database
+echo "CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};" > /tmp/db_init.sql
 
-mysql_secure_installation <<_EOF_
+# Alter root user password and grant privileges
+echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';" >> /tmp/db_init.sql
+echo "GRANT ALL ON *.* TO 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD' WITH GRANT OPTION;" >> /tmp/db_init.sql
+echo "FLUSH PRIVILEGES;" >> /tmp/db_init.sql
 
-Y
-rootpass
-rootpass
-Y
-n
-Y
-Y
-_EOF_
+# Create a user and grant privileges for the specified database
+echo "CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';" >> /tmp/db_init.sql
+echo "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';" >> /tmp/db_init.sql
+echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';" >> /tmp/db_init.sql
+echo "FLUSH PRIVILEGES;" >> /tmp/db_init.sql
 
-#Add a root user on 127.0.0.1 to allow remote connexion
+# Start MySQL service
+service mysql start
 
-	echo "GRANT ALL ON *.* TO 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD'; FLUSH PRIVILEGES;" | mysql -uroot
-
-#Create database and user for wordpress
-	echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE; GRANT ALL ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD'; FLUSH PRIVILEGES;" | mysql -uroot
-
-#Import database
-mysql -uroot -p$MYSQL_ROOT_PASSWORD $MYSQL_DATABASE
+# Run the SQL script to initialize the database and set up privileges
+mysql -u root -p$MYSQL_ROOT_PASSWORD < /tmp/db_init.sql
 
 fi
 
-/etc/init.d/mysql stop
+# Shutdown MySQL after setup
+mysqladmin -u root -p$MYSQL_ROOT_PASSWORD shutdown
 
-exec "$@"
+# Clean up temporary SQL script
+rm -rf /tmp/db_init.sql
+
+# Start MySQL server safely
+exec mysqld_safe
